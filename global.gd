9795272b1e577cf31ffd8f6750ed18a33d713278
Ethan <etjohn2345@hotmail.com>
2025-05-30 11:08:55 -0400

    added TODO

--- global.gd
+++ global.gd
@@ -4,3 +4,5 @@ var is_dragging := false
 var blank_texture := preload("res://Art/UAPrototype/UI/blank_item.png")
 var mode := &"default"
 var player_scale := Vector2(1.0, 1.0)
+
+#TODO: add items and recipes based on the spreadsheet
