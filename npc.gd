1b2d3626d403e7d271498c4dcdb85ae27644b210
Ethan <etjohn2345@hotmail.com>
2025-10-07 12:35:43 -0400

    Update npc.gd

--- npc.gd
+++ npc.gd
@@ -6,5 +6,25 @@ class_name NPC extends CharacterBody2D
 # Other (scared, runs off / opens a passageway / etc.)
 
 #TODO: consider different types of conversations and how to easily swap between them (consider export)
+
+
+func open_dialogue(message : String) -> void:
+	pass
+	# window.open
+
+func close_dialogue() -> void:
+	pass
+	# window.resetandclose()
+
 func _on_interact_area_npc_talk() -> void:
+	pass
+	#open_dialogue("How can I help you?")
+
+func _on_interact_area_grab():
+	pass
+
+func _on_interact_area_cut():
+	pass
+
+func _on_interact_area_dropper():
 	pass
