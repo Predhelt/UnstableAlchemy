d9c9873ff0e4c1393825d1e4f3f7c84cbadc4598
Ethan <etjohn2345@hotmail.com>
2026-02-06 17:07:53 -0500

    Art, Menu, and file structuring.
    Moved NPC class scripts to "NPCs" folder
    Added new icon art.
    Added grass tileset.
    Added folder "Main Menu" with title screen and level select scenes.
    Removed display_area_trigger from physics layer collisions.

---
+++ Maps/Main Menu/title_screen.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+
+
+
+func _on_button_play_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_settings_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_button_exit_pressed() -> void:
+	get_tree().quit()
