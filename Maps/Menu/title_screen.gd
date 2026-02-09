2509d4363e73787ed2dd991898e6e91f2e697a89
Ethan <etjohn2345@hotmail.com>
2026-02-09 15:19:46 -0500

    Art and Organization Updates
    Added "museum_main" map to display different types of objects.
    Added rock_wall_hole image.
    Updated tileset_grass to remove outlines on background objects.
    Moved main menu UI maps to Maps\Menu\.
    Added buttons and text to TitleScreen.
    Added buttons and layout nodes to LevelSelect.
    Added collision layer StaticBody.
    Moved boulder_pushable and wall_small_hole to Objects\Collisions\

--- Maps/Menu/title_screen.gd
+++ Maps/Menu/title_screen.gd
@@ -1,11 +1,13 @@
 extends Control
 
+#@export var levels
 
-
-
+## Opens the level that was selected. Returns whether or not the level was opened successfuly.
+func open_level() -> bool:
+	return false
 
 func _on_button_play_pressed() -> void:
-	pass # Replace with function body.
+	pass
 
 
 func _on_button_settings_pressed() -> void:
