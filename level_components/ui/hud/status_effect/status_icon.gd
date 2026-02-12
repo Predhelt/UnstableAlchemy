57e81b0beb0fc5a98dc0b8386d376699daa74b8f
Ethan <etjohn2345@hotmail.com>
2026-02-12 11:05:50 -0500

    Shrink Puzzle Completion
    Added grow and shrink status effect icons
    Added Panel as StatusIcon scene root.
    Updated red_berry_bush combination material.
    shrink_puzzle is now completable from scratch.

--- level_components/ui/hud/status_effect/status_icon.gd
+++ level_components/ui/hud/status_effect/status_icon.gd
@@ -1,4 +1,4 @@
-class_name StatusIcon extends TextureRect
+class_name StatusIcon extends Panel
 
 var has_duration : bool
 var effect : StatusEffect
@@ -6,17 +6,17 @@ var effect : StatusEffect
 func _process(delta: float) -> void:
 	if global.mode != &"default":
 		return
-	if has_duration and $ProgressBar.value < $ProgressBar.max_value:
-		$ProgressBar.value += delta
+	if has_duration and $TextureRect/ProgressBar.value < $TextureRect/ProgressBar.max_value:
+		$TextureRect/ProgressBar.value += delta
 
 func reset_effect(se: StatusEffect):
 		effect = se
-		texture = se.icon
+		$TextureRect.texture = se.icon
 		if se.duration != -1:
 			has_duration = true
-			$ProgressBar.max_value = se.duration
-			$ProgressBar.value = 0
-			$ProgressBar.visible = true
+			$TextureRect/ProgressBar.max_value = se.duration
+			$TextureRect/ProgressBar.value = 0
+			$TextureRect/ProgressBar.visible = true
 		else:
 			has_duration = false
-			$ProgressBar.visible = false
+			$TextureRect/ProgressBar.visible = false
