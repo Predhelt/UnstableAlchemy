b448ee58d27294d3721ffbbb8badc24215fea8db
Ethan <etjohn2345@hotmail.com>
2026-03-27 12:45:30 -0400

    Level Load Path Fixes, HTML Export
    Fixed broken level links.
    Set up HTML5 export properly for itch.io.
    Excluded notes from exports.

--- level_components/objects/triggers/trigger_area_level_change.gd
+++ level_components/objects/triggers/trigger_area_level_change.gd
@@ -3,6 +3,9 @@ extends Area2D
 
 @export_dir var level_path : String
 
+#func _ready() -> void:
+	#level_path = level_path.replace('.remap','')
+
 ## When player enters the area, load the level being stored.
 func _on_body_entered(_body: Node2D) -> void:
 	#call_deferred()
