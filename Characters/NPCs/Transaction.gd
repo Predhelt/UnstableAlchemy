be19d0307ea8ccb95988b24af35a756e44c218c9
Ethan <etjohn2345@hotmail.com>
2026-03-29 10:31:37 -0400

    Delete Characters directory
    part of process of merging folders with different capitalization

--- Characters/NPCs/Transaction.gd
+++
@@ -1,9 +0,0 @@
-class_name Transaction extends Resource
-
-var id : int # Local id of trade offer for given merchant
-
-@export var items_selling : Array[Item]
-@export_range(0,99) var items_selling_amount : Array[int]
-
-@export var items_buying : Array[Item]
-@export_range(0,99) var items_buying_amount : Array[int]
