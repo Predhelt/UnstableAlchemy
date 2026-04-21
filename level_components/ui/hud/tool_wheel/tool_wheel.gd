746ab4d1fce4ef96032210b51a44c3957b911940
Ethan <etjohn2345@hotmail.com>
2026-04-21 12:25:15 -0400

    Added Tool Wheel "change tool" button
    - Added action key "change_tool"
    - Changed text to add ingredient to dropper from "+" to "Add"
    - Changed outline size of tool wheel labels

--- level_components/ui/hud/tool_wheel/tool_wheel.gd
+++ level_components/ui/hud/tool_wheel/tool_wheel.gd
@@ -2,6 +2,7 @@ extends GridContainer
 
 signal set_dropper_item()
 
+## The currently selected tool. Tool names are "hand", "blade", and "dropper"
 var selected_tool := ""
 
 var has_blade := false
@@ -19,12 +20,14 @@ func _ready() -> void:
 	$Slot1.button_pressed = true
 	$CurrentTool/AddItemButton.visible = false
 	tooltip_text = selected_tool
-	$CurrentTool/HotkeyLabel.text = (
-		InputMap.action_get_events("tool_wheel")[0].as_text().replace(' - Physical',''))
 	_close_tool_selection()
 	set_ui()
 
 func set_ui() -> void:
+	$CurrentTool/HotkeyLabel.text = (
+		InputMap.action_get_events("tool_wheel")[0].as_text().replace(' - Physical',''))
+	$CurrentTool/ChangeLabel.text = ("%s->" %
+		InputMap.action_get_events("change_tool")[0].as_text().replace(' - Physical',''))
 	if not has_blade and not has_dropper:
 		$CurrentTool/HotkeyLabel.visible = false
 		$CurrentTool.disabled = true
@@ -37,6 +40,24 @@ func _input(event: InputEvent) -> void:
 		_close_tool_selection()
 	if event.is_action_pressed("tool_wheel"):
 		toggle_tool_selection()
+	if event.is_action_pressed("change_tool"):
+		change_tool()
+
+## Change the current tool to the next one in order. The logic is done manually
+## since there are only 3 options.
+func change_tool() -> void:
+	if selected_tool == "hand":
+		if has_blade:
+			set_tool_to_blade()
+		elif has_dropper:
+			set_tool_to_dropper()
+	elif selected_tool == "blade":
+		if has_dropper:
+			set_tool_to_dropper()
+		else:
+			set_tool_to_hand()
+	elif selected_tool == "dropper":
+		set_tool_to_hand() # Next is always hand.
 
 ## Toggle visibility of blade tool slot
 func set_blade_enabled(is_enabled : bool) -> void:
@@ -59,47 +80,52 @@ func set_dropper_enabled(is_enabled : bool) -> void:
 			$CurrentTool.disabled = true
 
 func set_tool_to_hand() -> void:
-	_on_slot_1_toggled(true)
+	if selected_tool == "hand":
+		return
+	
+	selected_tool = "hand"
+	tooltip_text = selected_tool
+	if Global.focused_node:
+		Global.focused_node.tool_updated("hand")
+	$CurrentTool.icon = $Slot1.icon
+
+func set_tool_to_blade() -> void:
+	if selected_tool == "blade":
+		return
+	
+	selected_tool = "blade"
+	tooltip_text = selected_tool
+	if Global.focused_node:
+		Global.focused_node.tool_updated("blade")
+	$CurrentTool.icon = $Slot2.icon
+
+func set_tool_to_dropper() -> void:
+	if selected_tool == "dropper":
+		return
+	
+	selected_tool = "dropper"
+	tooltip_text = selected_tool
+	if Global.focused_node:
+		Global.focused_node.tool_updated("dropper")
+	$CurrentTool.icon = $Slot3.icon
+	$CurrentTool/AddItemButton.visible = true
 
 func _on_slot_1_toggled(toggled_on: bool) -> void:
 	if toggled_on:
-		if selected_tool == "hand":
-			return
-		
-		selected_tool = "hand"
-		tooltip_text = selected_tool
-		if Global.focused_node:
-			Global.focused_node.tool_updated("hand")
-		$CurrentTool.icon = $Slot1.icon
-		toggle_tool_selection()
+		set_tool_to_hand()
+		_close_tool_selection()
 
 
 func _on_slot_2_toggled(toggled_on: bool) -> void:
 	if toggled_on:
-		if selected_tool == "blade":
-			return
-		
-		selected_tool = "blade"
-		tooltip_text = selected_tool
-		if Global.focused_node:
-			Global.focused_node.tool_updated("blade")
-		$CurrentTool.icon = $Slot2.icon
-		toggle_tool_selection()
+		set_tool_to_blade()
+		_close_tool_selection()
 
 
 func _on_slot_3_toggled(toggled_on: bool) -> void:
 	if toggled_on:
-		if selected_tool == "dropper":
-			return
-		
-		selected_tool = "dropper"
-		tooltip_text = selected_tool
-		if Global.focused_node:
-			Global.focused_node.tool_updated("dropper")
-		$CurrentTool.icon = $Slot3.icon
-		toggle_tool_selection()
-		$CurrentTool/AddItemButton.visible = true
-	
+		set_tool_to_dropper()
+		_close_tool_selection()
 	else:
 		dropper_item = null
 		$CurrentTool/AddItemButton.visible = false
