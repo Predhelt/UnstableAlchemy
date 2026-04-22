367b1cd9a9de69dbe821bf8c2a5c03e89c9c74ef
Ethan <etjohn2345@hotmail.com>
2026-04-22 11:06:26 -0400

    3-1 puzzlemap update
    - 3-1 inherited puzzlemap updated to match 3-1 advanced puzzle.
    - Changed LabelLevelName script to display current_scene.scen_file_path name.

--- level_components/ui/hud/label_level_name.gd
+++ level_components/ui/hud/label_level_name.gd
@@ -1,4 +1,7 @@
 extends Label
 
 func _ready() -> void:
-	text = Global.current_level_path.split("/")[-1].split(".")[0].replace("_", " ")
+	#if Global.current_level_path != "":
+		#text = Global.current_level_path.split("/")[-1].split(".")[0].replace("_", " ")
+	#else:
+	text = get_tree().current_scene.scene_file_path.split("/")[-1].split(".")[0].replace("_", " ")
