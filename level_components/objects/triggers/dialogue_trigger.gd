99c54da62cd17b5effaea88149a67d37bfbd1e51
Ethan <etjohn2345@hotmail.com>
2026-05-07 11:32:35 -0400

    Update 1-ruins_entrance map
    - Updated ending map label text
    - dialogue_trigger: Added TimerRepeat started after initial timer.
    - 1-ruins_entrance: Condensed the tilemap further.

--- level_components/objects/triggers/dialogue_trigger.gd
+++ level_components/objects/triggers/dialogue_trigger.gd
@@ -3,8 +3,11 @@ class_name DialogueTrigger extends Area2D
 ## The message to be displayed by default if no custom message conditions are set up.
 @export var message : String
 ## Time the player needs (in seconds) to be in the area before the message is displayed.
-## Leaving the area resets the time.
-@export var wait_time : float = 0.0
+## After triggering, starts repeating timer. Leaving the area resets the time.
+@export var initial_wait_time : float = 0.0
+## Time the player needs (in seconds) to be in the area before the message is displayed.
+## Leaving the area stops the timer.
+@export var repeat_wait_time : float = 0.0
 ## Reference to the player that is being tracked for message display.
 var player_ref : Character
 
@@ -15,14 +18,17 @@ func show_message() -> void:
 ## Starts the timer with the given wait time before showing the message.
 ## If there is no wait time, shows the message.
 func start_trigger() -> void:
-	if wait_time > 0:
-		$Timer.start(wait_time)
-	elif wait_time == 0:
+	if initial_wait_time > 0:
+		$TimerInitial.start(initial_wait_time)
+	elif initial_wait_time == 0:
 		show_message()
+		if repeat_wait_time > 0:
+			$TimerRepeat.start(repeat_wait_time)
 
 ## Stops the timer.
 func stop_trigger() -> void:
-	$Timer.stop()
+	$TimerInitial.stop()
+	$TimerRepeat.stop()
 
 ## Start the timer if the player enters the area.
 func _on_body_entered(body: Node2D) -> void:
@@ -32,10 +38,14 @@ func _on_body_entered(body: Node2D) -> void:
 
 ## Cancel the timer if the player enters the area.
 func _on_body_exited(body: Node2D) -> void:
-	if not $Timer.is_stopped() and body == player_ref:
+	if (not $TimerInitial.is_stopped() or not $TimerRepeat.is_stopped()) and body == player_ref:
 		player_ref = null
 		stop_trigger()
 
 
-func _on_timer_timeout() -> void:
+func _on_timer_initial_timeout() -> void:
+	show_message()
+	$TimerRepeat.start(repeat_wait_time)
+
+func _on_timer_repeat_timeout() -> void:
 	show_message()
