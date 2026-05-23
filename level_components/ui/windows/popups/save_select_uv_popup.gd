a72581c793aa6360b175aadc56e27cedd1eda42a
Ethan <etjohn2345@hotmail.com>
2026-05-23 00:35:09 -0400

    Update Title Screen
    Title Screen:
    - Added level_select as page in TitleScreen scene.
    - Changed back button from level select to a regular button.
    - Added back button functionality for PlayTypePage.
    - Added save_select_uv_popup as child node in tiltle screen.
    Changed "Prototype" to "Demo" in version name.
    - Removed level_select scene and script.
    - save_select_uv_popup added logic to hide buttons based on available save data.
    - Fixed button press and hover sounds.
    - Removed several old puzzle maps.

--- level_components/ui/windows/popups/save_select_uv_popup.gd
+++ level_components/ui/windows/popups/save_select_uv_popup.gd
@@ -1,13 +1,34 @@
 ## Changes to the "Level Select" page. Loads only the user variables based on the selected option.
 extends PopupMenu
 
+var save_index: int = 0
+var autosave_index: int = 1
+#var no_save_index: int = 2
+
+func _ready() -> void:
+	var has_save_data: bool = false
+	var has_autosave_data: bool = false
+	if FileAccess.file_exists("savegame.save"):
+		has_save_data = true
+	if FileAccess.file_exists("autosavegame.save"):
+		has_save_data = true
+	
+	if not has_save_data and not has_autosave_data:
+		return
+	if not has_save_data:
+		remove_item(0)
+		save_index = -1
+		autosave_index = 0
+		#no_save_index = 1
+	if not has_autosave_data:
+		remove_item(1)
+		autosave_index = -1
+		#no_save_index = 1
 
 func _on_index_pressed(index: int) -> void:
-	if index == 0: # Manual save data
+	if index == save_index: # Manual save data
 		Global.load_user_variables("user://save")
-		Global.change_scene("res://maps/menu/level_select.tscn")
-	elif index == 1: # Autosave data
+	elif index == autosave_index: # Autosave data
 		Global.load_user_variables("user://autosave")
-		Global.change_scene("res://maps/menu/level_select.tscn")
-	elif index == 2: # No save data
-		Global.change_scene("res://maps/menu/level_select.tscn")
+	#elif index == no_save_index: # No save data
+		#pass
--- maps/menu/title_screen.gd
+++ maps/menu/title_screen.gd
@@ -1,11 +1,15 @@
 extends Control
 
 var save_select_popup_ref : PackedScene = preload("res://level_components/ui/windows/popups/save_select_popup.tscn")
-var save_select_uv_popup_ref : PackedScene = preload("res://level_components/ui/windows/popups/save_select_uv_popup.tscn")
+## Which page is currently being displayed.
+var current_page_ref: Control
+## Which page of the title screen the back button will lead to.
+var previous_page_ref: Control
 
 func _ready() -> void:
-	$LabelVersion.text = "Version: %s (Prototype)" % ProjectSettings.get_setting("application/config/version")
+	$LabelVersion.text = "Version: %s (Demo)" % ProjectSettings.get_setting("application/config/version")
 	UserVariables.reset_variables()
+	$PlayTypePage/ButtonLevelSelect/SaveSelectUVPopup.position = $PlayTypePage/ButtonLevelSelect.global_position
 
 ## Opens the level that was selected. Returns whether or not the level was opened successfuly.
 func open_level() -> bool:
@@ -41,11 +45,35 @@ func _on_button_entered() -> void:
 
 ## Open the menu buttons for selecting what to play
 func _on_button_play_pressed() -> void:
+	$AudioStreams/AudioStreamPlayer.play()
+	$AudioStreams/AudioStreamPlayer["parameters/switch_to_clip"] = "press"
 	$MainPage.visible = false
+	previous_page_ref = $MainPage
+	current_page_ref = $PlayTypePage
+	$ButtonBack.visible = true
 	$PlayTypePage.visible = true
 
 
 func _on_button_level_select_pressed() -> void:
-	var save_uv_select : PopupMenu = save_select_uv_popup_ref.instantiate()
-	add_child(save_uv_select)
-	save_uv_select.position = $PlayTypePage/ButtonLevelSelect.global_position
+	$AudioStreams/AudioStreamPlayer.play()
+	$AudioStreams/AudioStreamPlayer["parameters/switch_to_clip"] = "press"
+	$PlayTypePage/ButtonLevelSelect/SaveSelectUVPopup.show()
+
+
+func _on_save_select_uv_popup_index_pressed(_index: int) -> void:
+	$AudioStreams/AudioStreamPlayer.play()
+	$AudioStreams/AudioStreamPlayer["parameters/switch_to_clip"] = "press"
+	$PlayTypePage.visible = false
+	previous_page_ref = $PlayTypePage
+	current_page_ref = $LevelSelectPage
+	$ButtonBack.visible = true
+	$LevelSelectPage.visible = true
+
+
+func _on_button_back_pressed() -> void:
+	$AudioStreams/AudioStreamPlayer.play()
+	$AudioStreams/AudioStreamPlayer["parameters/switch_to_clip"] = "press"
+	current_page_ref.visible = false
+	if previous_page_ref == $MainPage:
+		$ButtonBack.visible = false
+	previous_page_ref.visible = true
