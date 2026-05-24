e50ba2b86ed9fa2733998269c91bc79b2801beff
Ethan <etjohn2345@hotmail.com>
2026-05-24 13:16:51 -0400

    Settings Tab Fix, Update Project
    - settings_menu: Fixed tab containers not being hidden in exported projects.
    - Game window is no longer resizeable due to image sharpness issues on different resolutions.

--- level_components/ui/windows/options/settings_menu.gd
+++ level_components/ui/windows/options/settings_menu.gd
@@ -1,5 +1,10 @@
 extends Window
 
+func _ready() -> void:
+	# Tabs are not hidden or disabled when exported if only set in editor, so this is to confirm.
+	$TabContainer.set_tab_hidden(1, true)
+	$TabContainer.set_tab_hidden(4, true)
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
 		_on_close_requested()
