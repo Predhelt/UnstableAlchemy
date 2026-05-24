2f0d2753f7231f913187a0d1af66005493b24b5c
Ethan <etjohn2345@hotmail.com>
2026-05-24 11:59:26 -0400

    Update trigger_area_autosave.gd
    - TriggerAreaAutosaves now are persistent nodes that instead of being freed upon trigger, track whether the area has saved yet.

--- level_components/objects/triggers/trigger_area_autosave.gd
+++ level_components/objects/triggers/trigger_area_autosave.gd
@@ -1,9 +1,20 @@
 extends Area2D
 
 ## Tracks if this trigger has already called to save the game.
-#var has_saved : bool = false
+var has_saved : bool = false
 
 func _on_body_entered(body: Node2D) -> void:
-	if body == Global.focused_node:
+	if not has_saved and body == Global.focused_node:
+		has_saved = true
 		Global.save_game("autosave")
-		queue_free()
+
+func save(_dir: String) -> Dictionary:
+	var save_dict = {
+		"filename" : get_scene_file_path(),
+		"name" : name,
+		"parent" : get_parent().get_path(),
+		"pos_x" : position.x, # Avoiding Vector2 for compatibility with JSON
+		"pos_y" : position.y,
+		"has_saved" : has_saved
+	}
+	return save_dict
