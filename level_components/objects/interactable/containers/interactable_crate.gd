27d8e839296c89e929f0a3e291d07cc1ebea2525
Ethan <etjohn2345@hotmail.com>
2026-05-26 14:16:23 -0400

    Add crate item count
    - Added functionality to interactable_crate to show the item count of the first (and usually only) item.

--- level_components/objects/interactable/containers/interactable_crate.gd
+++ level_components/objects/interactable/containers/interactable_crate.gd
@@ -6,3 +6,20 @@ extends InteractableObject
 func _ready() -> void:
 	$Sprite2D/ItemIcon.texture = contained_item_icon
 	super()
+	if item_quantities[0] < 0:
+		change_item_count_visibility(false)
+	else:
+		update_item_count(item_quantities[0])
+
+
+func collect_items(character: Character, interaction: Interaction, sfx_name: StringName) -> bool:
+	var b := super(character, interaction, sfx_name)
+	update_item_count(item_quantities[0])
+	return b
+
+## Changes the count representing how many items are left in the crate.
+func update_item_count(count: int):
+	$LabelCount.text = str(count)
+
+func change_item_count_visibility(visibility: bool) -> void:
+	$LabelCount.visible = visibility
