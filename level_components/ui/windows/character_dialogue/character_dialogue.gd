a93473a2b1bd3561d972ad4952d1bb2e9c12abb7
Ethan <etjohn2345@hotmail.com>
2026-05-27 13:30:03 -0400

    Update character_dialogue.gd
    Fixed improperly handling no matching dialogue effect.

--- level_components/ui/windows/character_dialogue/character_dialogue.gd
+++ level_components/ui/windows/character_dialogue/character_dialogue.gd
@@ -111,7 +111,8 @@ func execute_dialogue_effects(dialogue_effects : Array[String]) -> void: #NOTE: 
 				EventHandler.go_home()
 			"open_dialogue_door":
 				EventHandler.open_dialogue_door()
-		print("No Dialogue Effect with name " + effect)
+			_:
+				print("No Dialogue Effect with name " + effect)
 
 ## Opens the shop window and closes the current dialogue window.
 ## Uses transaction information from the character to set up the shop window.
