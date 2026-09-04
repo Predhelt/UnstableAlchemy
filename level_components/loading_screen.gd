7df37cc7b81afa11106dd93c627ba8958921b335
Ethan <etjohn2345@hotmail.com>
2026-09-04 10:52:17 -0400

    Update loading_screen.gd
    - Added return type bool for emit_particles().
    - Added var is_complete.
    - Removed print statement from load_threaded_request().
    - Added check during _process() for is_complete.
    - Awaits particle emissions.
    - Disables SFX audio bus while particles are being emitted.

--- level_components/loading_screen.gd
+++ level_components/loading_screen.gd
@@ -7,6 +7,8 @@ var particles : Array[String] = [
 ## Tracks if particles have been emitted already during a load.
 var particles_emitted : bool = false
 
+var is_complete: bool = false
+
 signal loading_completed
 
 func _ready() -> void:
@@ -14,26 +16,34 @@ func _ready() -> void:
 		print("ERROR: no scene to be loaded")
 	$LabelLoadDescription.text = "loading level"
 	connect("loading_completed", Global._on_loading_screen_completed)
-	print(ResourceLoader.load_threaded_request(Global.next_scene_path))
+	ResourceLoader.load_threaded_request(Global.next_scene_path)
 
 func _process(_delta: float) -> void:
 	var progress: Array = []
 	ResourceLoader.load_threaded_get_status(Global.next_scene_path, progress)
 	$HSlider.value = progress[0]*100
 	
-	if progress[0] == 1:
+	if not is_complete and progress[0] == 1:
+		is_complete = true
 		if not particles_emitted:
-			emit_particles()
+			await emit_particles()
+			particles_emitted = true
 		loading_completed.emit()
 
-## Emit particles durin loading so that any first-time emission lag is removed.
-func emit_particles():
+## Emit particles during loading so that any first-time emission lag is removed.
+func emit_particles() -> bool:
 	$LabelLoadDescription.text = "launching particles"
 	$HSlider.value = 0
-	var num_particles : float = len(particles)
-	var cur_particle : PackedScene
-	for i in range(len(particles)):
-		cur_particle = load(particles[i])
-		cur_particle.instantiate()
+	# Disable SFX audio bus
+	AudioServer.set_bus_mute(AudioServer.get_bus_index("SFX"), true)
+	var num_particles: float = len(particles)
+	var cur_particle: GPUParticles2D
+	for i in range(num_particles):
+		var packed_particle = load(particles[i])
+		cur_particle = packed_particle.instantiate()
+		add_child(cur_particle)
+		await cur_particle.tree_exited #NOTE: Plays each particle until finished.
 		$HSlider.value = (i/num_particles)*100
-	particles_emitted = true
+	# Enable SFX audio bus
+	AudioServer.set_bus_mute(AudioServer.get_bus_index("SFX"), false)
+	return true
